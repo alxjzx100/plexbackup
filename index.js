0059de89ebea84e160310e837f6ab9894bc4eed9
alx <alxjzx100@gmail.com>
2020-11-15 17:54:08 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+const findRemoveSync = require('find-remove');
+const sqlite3 = require('sqlite3').verbose();
+let moment = require('moment');
+const dir = '/home/plexbackup'; //Путь к папке с текстовыми файлами
+const plexDb = './com.plexapp.plugins.library.db';
+let file_name = moment().format('D-MM-Y H_mm_ss')+".txt";
+
+let result = findRemoveSync(dir, {age: {seconds: 86400*7}, extensions: '.txt', limit: 100});
+console.log('Deleted '+Object.keys(result).length+' file(s)');
+
+let db = new sqlite3.Database(plexDb, (err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    console.log('Connected to the plex database.');
+});
+
+db.serialize(() => {
+    db.each(`SELECT 
+  	metadata_items.title as Title,
+  	metadata_items.year as Year,
+  	media_parts.file AS Filename 
+  		FROM metadata_items JOIN media_parts 
+  							JOIN media_items 
+  		WHERE metadata_items.metadata_type=1 
+  		AND metadata_items.id=media_items.metadata_item_id 
+  		AND media_items.id=media_parts.media_item_id 
+  		ORDER BY metadata_items.title`, (err, row) => {
+        if (err) {
+            console.error(err.message);
+        }
+        fs.appendFileSync(dir+"/"+file_name, row.Title + ";" + row.Year + ";" + row.Filename+"\n");
+    });
+});
+
+db.close();
